4c63cec7ebaae7c30de042772ff54d51698ae25d
Víctor Gómez <victor.gomez@movetia.com>
2020-11-16 09:50:17 +0100

    [VGC] - Created Dashboard structure - Added Menu button and Characters, Comics and Events buttons

--- S.A.M/S.A.M/Dashboard/View/DashboardViewController.swift
+++ S.A.M/S.A.M/Dashboard/View/DashboardViewController.swift
@@ -9,35 +9,91 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
-    
-    @IBOutlet weak var headerView: UIView!
-    @IBOutlet weak var bottomHeaderConstraint: NSLayoutConstraint!
+
+    //Header Vars
     @IBOutlet weak var headerTitle: UILabel!
+    @IBOutlet weak var topHeaderTitle: NSLayoutConstraint!
     
+    
+    @IBOutlet weak var btnMenu: UIButton!
+    
+    @IBOutlet weak var viewScrollContainer: UIView!
+    
+    //Characters
+    @IBOutlet weak var viewContainerCharacters: UIView!
+    @IBOutlet weak var btnCharacters: UIButton!
+    
+    //Comics
+    @IBOutlet weak var viewContainerComics: UIView!
+    @IBOutlet weak var btnComics: UIButton!
+    
+    //Events
+    @IBOutlet weak var viewContainerEvents: UIView!
+    @IBOutlet weak var btnEvents: UIButton!
+    
+    private var router = DashboardRouter()
+    private var viewModel = DashboardViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.layoutIfNeeded()
-        self.headerView.frame = CGRect(x: 0, y: -44, width: self.headerView.frame.width, height: self.view.frame.height)
         
-        UIView.animate(withDuration: 1.5) {
-            self.bottomHeaderConstraint.constant = 0
-            
+        animatedTitleAndVisibleView()
+        
+        viewModel.bind(view: self, router: router)
+        
+        applyShadowAndCorner(to: viewContainerCharacters)
+        applyShadowAndCorner(to: viewContainerComics)
+        applyShadowAndCorner(to: viewContainerEvents)
+        
+    }
+    
+    
+    //MARK: - Buttons actions
+    
+    @IBAction func btnActionCharacters(_ sender: Any) {
+        //Navigate to Table Characters list
+    }
+    
+    @IBAction func btnActionComics(_ sender: Any) {
+        //Navigate to Table Comics list
+    }
+    
+    @IBAction func btnActionEvents(_ sender: Any) {
+        //Navigate to Table Events list
+    }
+    
+    
+    @IBAction func btnActionMenu(_ sender: Any) {
+        //Open menu with the rest of options
+    }
+    
+    private func applyShadowAndCorner(to view: UIView) {
+        view.layer.cornerRadius = 15
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowOffset = CGSize(width: 0, height: 5)
+        view.layer.shadowRadius = 10
+        
+    }
+    
+    //MARK: - Animation view
+    /// Title like LaunchScreen animated to top and make visible dashboard view
+    private func animatedTitleAndVisibleView() {
+        
+        self.viewScrollContainer.alpha = 0
+        self.btnMenu.alpha = 0
+        self.topHeaderTitle.constant = 200
+        
+        UIView.animate(withDuration: 1, delay: 0.5) {
+            self.topHeaderTitle.constant = 75
             self.view.layoutIfNeeded()
+        } completion: { completed in
+            UIView.animate(withDuration: 0.5) {
+                self.btnMenu.alpha = 1
+                self.viewScrollContainer.alpha = 1
+                self.view.layoutIfNeeded()
+            }
         }
-    
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
